47bcc124305b08ed03de7ff086e68cf5b92dc53a
moh3a <aitabdelmalekma@gmail.com>
2023-10-13 17:29:34 +0100

    Dynamic programming - Grid traveler

--- dynamic_prog/gridTraveler.js
+++ dynamic_prog/gridTraveler.js
@@ -1,3 +1,9 @@
+// ==========================================================
+//
+// GRID TRAVELER
+//
+// ==========================================================
+
 /**
  *
  * Say that you are a traveler on a 2D grid.
@@ -9,3 +15,35 @@
  * on a grid with dimensions m * s ?
  *
  * */
+
+const gridTraveler = (m, n) => {
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+};
+
+console.time("Grid traveler (6,6)");
+console.log(gridTraveler(6, 6));
+console.timeEnd("Grid traveler (6,6)");
+// console.log(gridTraveler(18, 18)); <- very slow! memoization is required
+
+// =========================================================
+//
+// GRID TRAVELER WITH MEMOIZATION
+//
+// =========================================================
+
+const memo_gridTraveler = (m, n, memo = {}) => {
+  const key = `${m},${n}`;
+  if (key in memo) return memo[key];
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  memo[key] =
+    memo_gridTraveler(m - 1, n, memo) + memo_gridTraveler(m, n - 1, memo);
+  return memo[key];
+};
+
+console.time("Memoized grid traveler (6,6)");
+console.log(memo_gridTraveler(6, 6));
+console.timeEnd("Memoized grid traveler (6,6)");
+console.log("Memoized grid traveler (18, 18):" + memo_gridTraveler(18, 18));
